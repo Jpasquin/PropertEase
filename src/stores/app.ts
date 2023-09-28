a61af5ae151d57a444bfa829a06d141de7129d0c
Jonathan <jonathandominicpasquin@gmail.com>
2023-09-28 15:51:41 -0400

    Firebase Storage

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,4 +1,5 @@
 import { equalTo, get, getDatabase, orderByChild, query, ref } from 'firebase/database';
+import { getStorage, ref as firebaseRef, getDownloadURL } from "firebase/storage";
 import { defineStore } from 'pinia';
 
 export const useAppStore = defineStore('app', {
@@ -21,11 +22,33 @@ export const useAppStore = defineStore('app', {
       // Convert the snapshot to an array of objects
       const listings: any = [];
       listingListSnapshot.forEach(childSnapshot => {
-        listings.push(childSnapshot.val());
+        const listingId = childSnapshot.key; // This will give you the ID, e.g., "001"
+        const listingData = childSnapshot.val();
+        
+        // Add the ID to the listing object
+        listingData.id = listingId;
+        
+        listings.push(listingData);
       });
     
       console.log(listings);
       return listings;
+    },
+
+    async getListingImage(listingId: string): Promise<string> {
+      console.log('hello');
+      const storage = getStorage();
+      const imagePath = `listings-images/${listingId}.webp?alt=media`;
+      console.log(imagePath)
+      const imageRef = firebaseRef(storage, imagePath);
+  
+      try {
+          const url = await getDownloadURL(imageRef);
+          return url;
+      } catch (error) {
+          console.error("Failed to fetch image:", error);
+          throw error;
+      }
     }
   }
 });
