1e7da7dde5d130b9d8902a14343ac79af109b332
Jonathan <jonathandominicpasquin@gmail.com>
2023-10-02 19:18:26 -0400

    Layout fixes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       class="border-4 border-black bg-white"
       style="border-bottom: 1px solid #e9e9e9"
     >
-      <q-toolbar class="h-[80px] px-6 min-w-[400px] max-w-[1800px] m-auto">
+      <q-toolbar class="h-[80px] px-6 min-w-[400px] max-w-[1800px] justify-center sm:justify-start m-auto">
         <div
           class="text-[#2AAA8A] font-bold text-3xl tracking-[-1.5px] cursor-pointer"
           @click="$router.push('/')"
@@ -12,7 +12,10 @@
           PropertEase
         </div>
 
-        <div class="absolute right-0 mr-[24px]">
+        <div
+          class="absolute right-0 mr-[24px]" 
+          v-if="showDiv"
+        >
           <q-btn
             v-if="!authStore.isSignedIn"
             flat
@@ -48,6 +51,15 @@
             </q-menu>
           </q-btn>
         </div>
+
+        <q-footer
+          v-if="!showDiv"
+          class="border-4 border-black bg-white"
+          style="border-top: 1px solid #e9e9e9"
+        >
+          <q-toolbar class="h-[80px] px-6 min-w-[400px] m-auto text-black">
+          </q-toolbar>
+        </q-footer>
       </q-toolbar>
     </q-header>
 
@@ -58,17 +70,33 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted, computed } from 'vue';
 import { useAuthStore } from 'stores/auth';
 
 const authStore = useAuthStore();
 
-const userInitials = () => {
+const windowWidth = ref(window.innerWidth);
+
+const updateWidth = () => {
+    windowWidth.value = window.innerWidth;
+};
+
+const showDiv = computed(() => windowWidth.value > 640);
+
+const userInitials = computed(() => {
   if (authStore.user) {
     return (
       authStore.user.firstName[0] + authStore.user.lastName[0]
     ).toUpperCase();
   }
   return null;
-};
+});
+
+onMounted(() => {
+    window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('resize', updateWidth);
+});
 </script>
