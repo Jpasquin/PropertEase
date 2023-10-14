4ae726cd350c195fe4a46973805a989daebcc55c
Andrei Mihaescu <andreimihaescu@hotmail.com>
2023-10-14 13:19:03 -0400

    further work on script of search filter

--- src/components/FilterSearch.vue
+++ src/components/FilterSearch.vue
@@ -1,17 +1,22 @@
 <template>
+  <!-- switch search to searchQuery? -->
   <q-input
-    v-model="text"
+    type="text"
+    v-model="search"
     rounded
     outlined
     color="customteal"
     bottom-slots
-    label="Search"
+    label="Search a city"
     class="rounded absolute top-15% left-35% right-35%"
   >
     <template v-slot:append>
-      <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+      <q-icon name="close" @click="search = ''" class="cursor-pointer" />
     </template>
   </q-input>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import ListingItem from '../components/ListingItem.vue';
+</script>
