cb4ca0ff67b4c5cc126bff47a3617659a3be6b6f
Youssef Ouakaa <youssef.ouakaa2@gmail.com>
2023-10-21 12:25:50 -0400

    added broker listings page

---
+++ src/pages/BrokerPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <listing-list :amount="10" />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import ListingList from 'components/ListingList.vue';
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -17,6 +17,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/SettingsPage.vue') }],
   },
+  {
+    path: '/broker',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/BrokerPage.vue') }],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
