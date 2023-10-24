bd398d09d85c777b471d53826c285e7d5c65c5e5
Andrei Mihaescu <andreimihaescu@hotmail.com>
2023-10-24 12:34:29 -0400

    search filter work. also changed some firebase elements

--- src/boot/firebaseConnection.ts
+++ src/boot/firebaseConnection.ts
@@ -1,27 +1,32 @@
-import { boot } from 'quasar/wrappers'
+import { boot } from 'quasar/wrappers';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getDatabase } from 'firebase/database';
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async (/* { app, router, ... } */) => {
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+export default boot(async ({ app }) => {
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBH3QuItRKMv2kxBtOaH9yMCuojfmYP5zc",
-  authDomain: "propertease-5ff7d.firebaseapp.com",
-  projectId: "propertease-5ff7d",
-  storageBucket: "propertease-5ff7d.appspot.com",
-  messagingSenderId: "710347219343",
-  appId: "1:710347219343:web:91442d0aaeafc536ffebfa",
-  measurementId: "G-JK7J8WTBR5"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBH3QuItRKMv2kxBtOaH9yMCuojfmYP5zc',
+    authDomain: 'propertease-5ff7d.firebaseapp.com',
+    projectId: 'propertease-5ff7d',
+    storageBucket: 'propertease-5ff7d.appspot.com',
+    messagingSenderId: '710347219343',
+    appId: '1:710347219343:web:91442d0aaeafc536ffebfa',
+    measurementId: 'G-JK7J8WTBR5',
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-})
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(firebaseApp);
+  const db = getDatabase(firebaseApp);
+
+  // Add the database instance to the app's global properties
+  app.config.globalProperties.$db = db;
+});
